676e8b0f6e1f9a7eea393c243b61b05bb853220b
Christian Leonardo <pleasefloyd73@gmail.com>
2024-02-16 19:10:19 -0300

    ✨ feat: create logo component

---
+++ src/components/logo.tsx
@@ -0,0 +1,9 @@
+import { Pizza } from "lucide-react"
+
+export const Logo = () => {
+	return (
+		<div className="flex w-fit items-center gap-2 overflow-hidden p-1">
+			<Pizza className="size-6 transition hover:scale-110" />
+		</div>
+	)
+}
