751d38c0fddf9196a7d4719747930a02d1739a95
Christian Leonardo <pleasefloyd73@gmail.com>
2024-02-20 17:34:01 -0300

    chore: fix page import

--- src/lib/router-dom/routes.tsx
+++ src/lib/router-dom/routes.tsx
@@ -2,11 +2,11 @@ import { createBrowserRouter } from "react-router-dom"
 
 import { AppLayout } from "@/pages/_layouts/app"
 import { AuthLayout } from "@/pages/_layouts/auth"
-import { NotFoundPage } from "@/pages/404"
 import { Dashboard } from "@/pages/app/dashboard/dashboard"
 import { Orders } from "@/pages/app/orders/orders"
 import { SignIn } from "@/pages/auth/sign-in"
 import { SignUp } from "@/pages/auth/sign-up"
+import { NotFoundPage } from "@/pages/not-found"
 
 export const router = createBrowserRouter([
 	{
